ca20830b26a5712510f7f79f62248d6b0aaf41d8
Perry Birch <perrybirch@vizidrix.com>
2018-07-20 15:59:22 -0700

    Cicd (#1)
    * updated readme and removed commented src
    
    * build docs
    
    * coverage
    
    * cicd
    
    * update rust fmt
    
    * rustfmt
    
    * break out impls
    
    * baseline

--- README.md
+++ README.md
@@ -6,8 +6,43 @@ and, immediately, return a Result indicating success / failure of receipt.
 As a base primitive this should enable a message oriented variant of the
 inbound params to the familiar imperitive Result 'and_then' composition pattern.
 
+## Background
+
+A primary goal of the interface for ISink is to hide the implementation details
+of any sink.
+
+Some decisions were made in order to facilitate that:
+
+* Send takes Sink reference
+
+We don't want to consume the sink so that it can be reused to send many times.
+
+We also don't want to require the sender maintain a mutable reference to the sink.
+
+* Delegated Mutation
+
+StatefulSink requires that the creator manages mutation by taking ownership of the state type in it's struct.
+
+This means that mutation and thread-safety level concerns are pushed out to the implementors.
+
+* Send takes ownership of the sent value
+
+Similar to Delegated Mutation, this forces the sender to be responsible for sharing, thread-safety, etc.
+
+This also means that downstream recipients are explicitly decoupled from the originating source.
+
 ## Some Helpful References
 
 [Implementing Function Composition](https://users.rust-lang.org/t/implementing-function-composition/8255/2)
 
 [Railway Oriented Programming](https://fsharpforfunandprofit.com/rop/)
+
+## CI/CD
+
+[Building a Rust Project on CircleCI](https://abronan.com/building-a-rust-project-on-circleci/)
+
+> To Get RustFmt Options:
+
+````bash
+rustup run nightly -- rustfmt --help
+````
---
+++ src/statefulsink.rs
@@ -0,0 +1,90 @@
+use super::*;
+
+/// Sink implementation which owns an internal state that is made available to
+/// the provided handler when values are sent to it
+pub struct StatefulSink<'a, TState, TInput, TResult, TError>
+where
+    TState: Clone,
+{
+    state: TState,
+    handler: Box<Fn(TState, TInput) -> Result<TResult, TError> + 'a>,
+}
+
+impl<'a, TState, TInput, TResult, TError> StatefulSink<'a, TState, TInput, TResult, TError>
+where
+    TState: Clone,
+{
+    /// Builds a StatefulSink using the default for TState
+    pub fn new<F: 'a>(handler: F) -> Self
+    where
+        TState: Default,
+        F: Fn(TState, TInput) -> Result<TResult, TError> + 'a,
+    {
+        StatefulSink::with_state(TState::default(), handler)
+    }
+
+    /// Builds a StatefulSink using the TState provided
+    pub fn with_state<F: 'a>(state: TState, handler: F) -> Self
+    where
+        F: Fn(TState, TInput) -> Result<TResult, TError> + 'a,
+    {
+        StatefulSink {
+            state: state,
+            handler: Box::new(handler),
+        }
+    }
+}
+
+impl<'a, TState, TInput, TResult, TError> ISink
+    for StatefulSink<'a, TState, TInput, TResult, TError>
+where
+    TState: Clone,
+{
+    type TInput = TInput;
+    type TResult = TResult;
+    type TError = TError;
+
+    fn send(
+        &self,
+        input: <Self as ISink>::TInput,
+    ) -> Result<<Self as ISink>::TResult, <Self as ISink>::TError> {
+        (self.handler)(self.state.to_owned(), input)
+    }
+}
+
+#[cfg(test)]
+mod stateful_sink_tests {
+    use super::*;
+
+    use std::cell::RefCell;
+
+    #[test]
+    fn should_send_single_item_to_statefulsink() {
+        let sink = StatefulSink::<(), (), (), ()>::new(|_state, _item| Ok(()));
+
+        sink.send(()).unwrap();
+    }
+
+    #[test]
+    fn should_send_multiple_items_to_statefulsink() {
+        let sink = StatefulSink::<(), (), (), ()>::new(|_state, _item| Ok(()));
+
+        sink.send(()).unwrap();
+        sink.send(()).unwrap();
+    }
+
+    #[test]
+    fn should_update_state_on_send_given_mutable_type() {
+        let initial = RefCell::new(10);
+
+        let s =
+            StatefulSink::<&RefCell<usize>, usize, usize, ()>::with_state(&initial, |s, item| {
+                let mut value = s.borrow_mut();
+                *value += item;
+                Ok(value.to_owned())
+            });
+
+        assert_eq!(Ok(20), s.send(10));
+        assert_eq!(Ok(40), s.send(20));
+    }
+}
